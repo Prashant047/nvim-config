1b3b3db7113be3d4a119dab59b854abe1d63a313
Prashant Kumar <prash.kumar047@gmail.com>
2025-11-06 21:08:41 +0000

    remove unused theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,17 +1,5 @@
 return {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        no_italic = true,
-        no_bold = true,
-      })
-      -- vim.cmd("colorscheme catppuccin-mocha")
-    end
-  },
-  {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
